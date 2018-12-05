320f64884f658947d78c4cac08f379d08915f47e
Apache <apache@wlp-fn2187.its.bethel.edu>
2018-12-05 11:17:57 -0600

    added server changes

--- app/controllers/controller.py
+++ app/controllers/controller.py
@@ -3,8 +3,9 @@ import fnmatch
 import hashlib
 import base64
 import os
-# from thumbor import thumbor
+import thumbor.storages as storages
 from subprocess import call
+from Crypto.Cipher import AES
 
 from app import app
 
@@ -20,30 +21,34 @@ def clear_image_cache(image_path):
     def pad(s):
         return s + (16 - len(s) % 16) * "{"
 
+    def path_on_filesystem(self, path):
+        digest = hashlib.sha1(path.encode('utf-8')).hexdigest()
+        return "%s/%s/%s" % (
+            app.config['THUMBOR_RESULT_STORAGE_LOCATION'].rstrip('/'),
+            digest[:2],
+            digest[2:]
+        )
+
     # for prefix in ['http://www.bethel.edu', 'https://www.bethel.edu',
     #                'http://staging.bethel.edu', 'https://staging.bethel.edu',
     #                'http://thumbor.bethel.edu', 'https://thumbor.bethel.edu']:
     path = image_path
-    # digest = hashlib.sha1(path.encode('utf-8')).hexdigest()
-    # url = "%s/%s" % (generated_url, hashlib.md5(image).hexdigest())
-
-    path = "%s/%s/%s" % (app.config['THUMBOR_STORAGE_LOCATION'].rstrip('/'), digest[:2], digest[2:])
-    resp.append(digest)
+    resp.append(path)
+    encrypted_path = path_on_filesystem(path)
+    resp.append(encrypted_path)
 
     # remove the file at the path
     # call(['rm', path])
 
-    print resp
-
     # now the result storage
-    file_name = image_path.split('/')[-1]
-    matches = []
-    for root, dirnames, filenames in os.walk(app.config['THUMBOR_RESULT_STORAGE_LOCATION']):
-        for filename in fnmatch.filter(filenames, file_name):
-            matches.append(os.path.join(root, filename))
-    for match in matches:
-        call(['rm', match])
+    # file_name = image_path.split('/')[-1]
+    # matches = []
+    # for root, dirnames, filenames in os.walk(app.config['THUMBOR_RESULT_STORAGE_LOCATION']):
+    #     for filename in fnmatch.filter(filenames, file_name):
+    #         matches.append(os.path.join(root, filename))
+    # for match in matches:
+    #     call(['rm', match])
 
-    matches.extend(resp)
+    # matches.extend(resp)
 
-    return str(matches)
+    return str(resp)
