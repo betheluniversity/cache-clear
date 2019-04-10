15740d7c2bee9aef2dc4303aebfeb159ee50a8cd
Philip Gibbens <phg49389@bethel.edu>
2019-04-10 17:00:07 -0500

    Added some preliminary styling
    Still has some fine-tuning to do, but it’s passable for now.

--- app/forms/rpapi.py
+++ app/forms/rpapi.py
@@ -3,8 +3,26 @@ import re
 from flask import render_template
 from wtforms import Form, StringField, SubmitField, validators, RadioField, ValidationError
 
-# TODO: finish this list
-varnish_domains = ['www.bethel.edu']
+varnish_domains = [
+    'www.bethel.edu',
+    'cdn.bethel.edu',  # cdn[1-4] are all aliases; the actual backend vhost is listening for this request
+    'bsg.bethel.edu',
+    'bsa.bethel.edu',
+    'cas.bethel.edu',
+    'caps.bethel.edu',
+    'gs.bethel.edu',
+    'sem.bethel.edu',
+    'ems.bethel.edu',
+    'business-signage.bethel.edu',
+    'beacon.bethel.edu',
+    'bethelnet.bethel.edu',
+    'classifieds.bethel.edu',
+    'book-exchange.bethel.edu',
+    'maps.bethel.edu',
+    'mahara.bethel.edu',
+    'wufoo.bethel.edu',
+    'programs.bethel.edu',
+]
 
 
 #######################################################
@@ -15,20 +33,19 @@ def valid_purge_uri():
 
     def _uri(form, field):
         # This regex should match anything from www.google.com to https://cdn1.bethel.edu/resize/
-        uri_pattern = re.compile(r'^(https?://)?([A-Za-z0-9-]+\.[A-Za-z0-9-]+\.[A-Za-z0-9-]+)(/.*)?$')
+        uri = re.compile(r'^(https?://)?([A-Za-z0-9-]+\.[A-Za-z0-9-]+\.[A-Za-z0-9-]+)(/.*)?$').search(field.data)
 
         # First, assert that the value submitted is syntactically a URI
-        result = uri_pattern.search(field.data)
-        if result is None:
+        if uri is None:
             raise ValidationError('The path submitted is not a valid URI')
 
         # Second, assert that the domain is one of the domains behind Varnish
-        domain = result.group(2)
+        domain = uri.group(2)
         if domain not in varnish_domains:
             raise ValidationError('%s isn\'t behind Varnish' % domain)
 
         # Finally, assert that the URL is at least '/'
-        url = result.group(3)
+        url = uri.group(3)
         if url is None:
             raise ValidationError('The URL at the end of the path needs to have at least a "/" at the beginning')
 
@@ -42,21 +59,20 @@ def valid_ban_syntax():
 
         # Because someone will inevitably try to use an OR instead of just ANDs, I'm adding this block of code
         # before the syntax check.
-        or_pattern = re.compile(r'\|\|')
-        result = or_pattern.search(field_data)
-        if result:
+        contains_an_or = re.compile(r'\|\|').search(field_data)
+        if contains_an_or is not None:
             raise ValidationError('Varnish Bans only support doing logical ANDs, not ORs. '
                                   'You\'ll have to submit multiple Bans to achieve an OR.')
 
         _recursive_syntax_check(field_data.strip())
 
     def _recursive_syntax_check(expression):
-        and_pattern_result = re.compile(r'(.*?)&&(.*)').search(expression)
+        contains_an_and = re.compile(r'(.*?)&&(.*)').search(expression)
 
-        if and_pattern_result is not None:
+        if contains_an_and is not None:
             # If expression contains '&&', then it's a compound expression
-            left_comparison = and_pattern_result.group(1).strip()
-            right_expression = and_pattern_result.group(2).strip()
+            left_comparison = contains_an_and.group(1).strip()
+            right_expression = contains_an_and.group(2).strip()
 
             if right_expression == '':
                 raise ValidationError('Ban expressions can\'t end with an &&')
@@ -112,7 +128,8 @@ class RenderableForm(Form):
 class PurgeRefreshForm(RenderableForm):
     uri = StringField('Path:', [validators.DataRequired(), valid_purge_uri()])
     api_action = RadioField('Action:', [validators.DataRequired()],
-                            choices=[('purge', 'Purge'), ('refresh', 'Refresh')])
+                            choices=[('purge', 'Purge'), ('refresh', 'Refresh')],
+                            default='refresh')
     submit = SubmitField('Go')
 
 
